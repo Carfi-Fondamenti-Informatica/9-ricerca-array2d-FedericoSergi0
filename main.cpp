aff7fdf5c8058adaf433b5923ba0851f4a1e5063
FedericoSergi0 <91880033+FedericoSergi0@users.noreply.github.com>
2021-12-11 17:32:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int posizione;
+  char a[10][20], f[1][20];
+  for (int i=0; i<10; i++){
+    cin >> a[i];
+  }
+  cin >> f[0];
+  
+  if (find(a, f, posizione)){
+    cout << posizione << endl;
+  }else{
+    cout << "non presente" << endl;
+  }
+  
   return 0;
 }
